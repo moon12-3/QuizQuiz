c575c6bc1f23e06814912e4d37329f68c7ab3e02
pickLotto <s2123@e-mirim.hs.kr>
2022-08-02 15:38:34 +0900

    XML 속 내용을 DB에 집어넣음

--- app/src/main/java/com/example/quizquiz/MainActivity.kt
+++ app/src/main/java/com/example/quizquiz/MainActivity.kt
@@ -1,19 +1,89 @@
 package com.example.quizquiz
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import com.example.quizquiz.database.Quiz
 import com.google.android.material.navigation.NavigationView
-import androidx.room.Entity
+import com.example.quizquiz.database.QuizDatabase
+import org.w3c.dom.Element
+import javax.xml.parsers.DocumentBuilderFactory
 
 class MainActivity : AppCompatActivity() {
+
+    fun initQuizDataFromXMLFile() {
+        AsyncTask.execute { // 이 안에서 하면 메인 스레드가 아니라 셋 스레드에서 실행됨(ANR 방지)
+            val stream = assets.open("quizzes.xml")
+
+            val doBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+
+            val doc = doBuilder.parse(stream)
+
+            val quizzesFromXMLDoc = doc.getElementsByTagName("quiz")
+            val quizList = mutableListOf<Quiz>()
+            for(idx in 0 until quizzesFromXMLDoc.length) {
+                val e = quizzesFromXMLDoc.item(idx) as Element
+
+                val type = e.getAttribute("type")
+                val question = e.getElementsByTagName("question").item(0).textContent
+                val answer = e.getElementsByTagName("answer").item(0).textContent
+                val category = e.getElementsByTagName("category").item(0).textContent
+
+                when(type) {
+                    "ox" -> {
+                        quizList.add(
+                            Quiz(type = type,
+                            question = question,
+                            answer = answer,
+                            category = category)
+                        )
+                    }
+                    "multiple_choice" -> {
+                        var choices = e.getElementsByTagName("choice")
+                        var choiceList = mutableListOf<String>()
+                        for(idx in 0 until choices.length) {
+                            choiceList.add(choices.item(idx).textContent)
+                        }
+                        quizList.add(
+                            Quiz(
+                                type = type,
+                                question = question,
+                                answer = answer,
+                                category = category,
+                                guesses = choiceList))
+                    }
+                }
+                for(quiz in quizList) {
+                    db.quizDAO().insert(quiz)
+                }
+            }
+
+
+        }
+    }
+
+    lateinit var db : QuizDatabase
     lateinit var drawerToggle : ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        db = QuizDatabase.getInstance(this)
+
+        val sp : SharedPreferences = getSharedPreferences("pref", Context.MODE_PRIVATE)
+        if(sp.getBoolean("initialized", true)) {
+            initQuizDataFromXMLFile()
+            val editor = sp.edit()  // 데이터를 집어넣는 애
+            editor.putBoolean("initialized", false) // 다음부턴 실행이 되지 않는다.
+            editor.commit()
+        }
+
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
         val navView = findViewById<NavigationView>(R.id.drawer_nav_view)
 
@@ -68,4 +138,7 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
+
+
 }
